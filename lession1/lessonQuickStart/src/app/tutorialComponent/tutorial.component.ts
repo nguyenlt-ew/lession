e575f404f9ee8b4cf8e816c69c74dd8005ed7ffe
nguyenlt-ew <nguyenlt@eurowindow.biz>
2020-12-13 20:22:02 +0700

    lession 3 commit structural directive

--- lession1/lessonQuickStart/src/app/tutorialComponent/tutorial.component.ts
+++ lession1/lessonQuickStart/src/app/tutorialComponent/tutorial.component.ts
@@ -8,5 +8,7 @@ import {Component} from '@angular/core';
 
 export class TutorialComponent{
     public title:string = "My Angular 2 tutorial binding";
-    
+    public showLineIf:boolean = false;
+    public _color = 'yellow';
+    public colors = ['red', 'blue', 'yellow'];
 }
